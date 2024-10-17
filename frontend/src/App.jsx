800a7e463e2377712a6718b2ea8dcb202c1f0bd7
rashid7089 <60935992+rashid7089@users.noreply.github.com>
2024-10-17 20:53:50 +0300

    f

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import SignUp from './router/auth'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Navbar from './navbar'
+
+// pages
+import AddProject from './router/addProject'
+import Auth from './router/auth'
+import Home from './router/home'
+import ViewProject from './router/viewProject'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <SignUp />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/add-project" element={<AddProject />} />
+      <Route path="/view-project" element={<ViewProject />} />
+    </Routes>
   )
 }
 
