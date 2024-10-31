744c44b075694da5d30ea580c827f9bcc8e72e68
Fahad Almutairi <88871860+iLawFD@users.noreply.github.com>
2024-10-31 19:20:26 +0300

    updates on home page

--- frontend/src/router/home/index.jsx
+++ frontend/src/router/home/index.jsx
@@ -1,15 +1,14 @@
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo1 from './logo1.png'; 
 
 import logo from './logo.jpg'; 
+import logo1 from './logo1.png'; 
 
 function ProjectShowcase() {
   const projects = [
-    { id: 1, title: 'UAV Obstacle Detection and Avoidance', company: 'TRS Technologies Inc.', img: logo },
+    { id: 1, title: 'UAV Obstacle Detection and Avoidance', company: 'TRS Technologies Inc.', img: logo1 },
     { id: 2, title: 'Swapp – The Clothing Swapping App', company: 'Swapp', img: logo },
     { id: 3, title: 'EDI Patio Awning Design Team 1', company: 'College of Engineering Facilities', img: logo },
-    
   ];
 
   return (
@@ -17,11 +16,15 @@ function ProjectShowcase() {
       {/* Navbar */}
       <nav className="navbar navbar-light bg-success">
         <div className="container-fluid">
-          
+          <span className="navbar-brand mb-0 h1 text-white">Project Showcase</span>
+          <div className="d-flex">
+            <Link to="/" className="text-white mx-2">Home</Link>
+            <Link to="/add-project" className="text-white">Add Project</Link>
+          </div>
         </div>
       </nav>
 
-      {}
+      {/* Filters */}
       <div className="container my-4">
         <div className="row">
           <div className="col-md-6">
@@ -43,14 +46,19 @@ function ProjectShowcase() {
         </div>
       </div>
 
-      {}
+      {/* Project Cards */}
       <div className="container">
         <div className="row">
           {projects.map((project) => (
             <div className="col-md-4 mb-4" key={project.id}>
               <Link to={`/project/${project.id}`} className="text-decoration-none text-dark">
-                <div className="card">
-                  <img src={project.img} className="card-img-top" alt={project.title} />
+                <div className="card h-100">
+                  <img 
+                    src={project.img} 
+                    className="card-img-top" 
+                    alt={project.title} 
+                    style={{ height: '200px', objectFit: 'cover' }} 
+                  />
                   <div className="card-body">
                     <h5 className="card-title">{project.title}</h5>
                     <p className="card-text">{project.company}</p>
