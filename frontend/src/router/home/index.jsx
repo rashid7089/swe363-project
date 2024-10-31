b9dd286ea615e5ab9e5d24de9aeee155cafbd484
Fahad Almutairi <88871860+iLawFD@users.noreply.github.com>
2024-10-31 19:13:50 +0300

    adding image

--- frontend/src/router/home/index.jsx
+++ frontend/src/router/home/index.jsx
@@ -1,9 +1,25 @@
-// import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo1 from './logo1.png'; 
+
+import logo from './logo.jpg'; 
 
 function ProjectShowcase() {
+  const projects = [
+    { id: 1, title: 'UAV Obstacle Detection and Avoidance', company: 'TRS Technologies Inc.', img: logo },
+    { id: 2, title: 'Swapp – The Clothing Swapping App', company: 'Swapp', img: logo },
+    { id: 3, title: 'EDI Patio Awning Design Team 1', company: 'College of Engineering Facilities', img: logo },
+    
+  ];
+
   return (
-    <div>      
+    <div className="bg-dark text-white min-vh-100">
+      {/* Navbar */}
+      <nav className="navbar navbar-light bg-success">
+        <div className="container-fluid">
+          
+        </div>
+      </nav>
 
       {}
       <div className="container my-4">
@@ -11,9 +27,9 @@ function ProjectShowcase() {
           <div className="col-md-6">
             <select className="form-select">
               <option>Major</option>
-              <option>CS</option>
+              <option>CHE</option>
               <option>SWE</option>
-              <option>COE</option>
+              <option>CS</option>
             </select>
           </div>
           <div className="col-md-6">
@@ -27,20 +43,22 @@ function ProjectShowcase() {
         </div>
       </div>
 
-      {/* Project Cards */}
+      {}
       <div className="container">
         <div className="row">
-          {/* Example Project Card */}
-          <div className="col-md-4 mb-4">
-            <div className="card">
-              <img src="/KFUPM-Seal.png" className="card-img-top" alt="w" />
-              <div className="card-body">
-                <h5 className="card-title">Project Title</h5>
-                <p className="card-text">Company/Team Name</p>
-              </div>
+          {projects.map((project) => (
+            <div className="col-md-4 mb-4" key={project.id}>
+              <Link to={`/project/${project.id}`} className="text-decoration-none text-dark">
+                <div className="card">
+                  <img src={project.img} className="card-img-top" alt={project.title} />
+                  <div className="card-body">
+                    <h5 className="card-title">{project.title}</h5>
+                    <p className="card-text">{project.company}</p>
+                  </div>
+                </div>
+              </Link>
             </div>
-          </div>
-          {/* Repeat for other projects */}
+          ))}
         </div>
       </div>
     </div>
