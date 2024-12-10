6df1a7e3bb5e0346219b47b39f80548d35afdfc5
rashid7089 <60935992+rashid7089@users.noreply.github.com>
2024-12-10 22:00:25 +0300

    login.js fixis errors

--- backend/routes/auth/login.js
+++ backend/routes/auth/login.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 const router = express.Router();
@@ -9,21 +9,21 @@ const router = express.Router();
 router.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log("email", email);
 
     if (!email || !password) {
       return res.status(400).json({ error: 'All fields are required' });
     }
-    
     const user = await User.findOne({ email });
-    if (!user) return res.status(404).json({ error: 'User not found' });
+    if (!user) return res.status(404).json({ errorField: "email", message: 'Email not found' });
 
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(401).json({ error: 'Invalid password' });
+    if (!match) return res.status(401).json({ errorField: "password", message: 'Invalid password' });
 
     const token = jwt.sign({ userId: user._id }, 'yourSecretKey', { expiresIn: '1h' });
-    res.json({ token });
+    res.json({ token, userId:user._id });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
